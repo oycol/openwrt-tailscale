1b269f659b23cbf07bfd52042a819bd85041fe0d
oycol <oycol527@outlook.com>
2022-05-04 15:09:36 +0800

    fix install script

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-version=`wget -qO- -t1 -T2 "https://api.github.com/repos/oycol/openwrt-tailscale/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'`
-curl -L -o /tmp/tailscale.tar.gz https://github.com/oycol/openwrt-tailscale/releases/download/$version/openwrt-tailscale-mipsle.tar.gz ;tar Cxvvf /  /tmp/tailscale.tar.gz
+version=`wget --no-check-certificate -qO- https://api.github.com/repos/oycol/openwrt-tailscale/tags | grep 'name' | cut -d\" -f4 | head -1`;
+curl -L -o /tmp/tailscale.tar.gz https://github.com/oycol/openwrt-tailscale/releases/download/$version/openwrt-tailscale-mipsle.tar.gz ;tar -zxvf /tmp/tailscale.tar.gz -C /;
+service tailscaled enable;
+service tailscaled start;
+tailscale help;
